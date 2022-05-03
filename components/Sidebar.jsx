ad1bb3d83473f525291a583dc73afa6c8a7e1c76
Anjola Adeuyi <57623705+anjola-adeuyi@users.noreply.github.com>
2022-05-03 10:53:19 +0100

    authentication

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -1,9 +1,32 @@
+import Image from "next/image"
 import { ConnectButton } from "web3uikit"
+
+const styles = {
+  container: ``,
+}
+
+const isAuthenticated = false;
 
 const Sidebar = () => {
   return (
-    <div>
-      <ConnectButton />
+    <div className={styles.container}>
+      <div className={styles.profile}>
+        {
+          isAuthenticated && (
+            <>
+              <div className={styles.profilePicContainer}>
+                <Image 
+                  // src="/static/images/profile.jpg"
+                  width={100}
+                  height={100}
+                  alt="profile"
+                  className={styles.profilePic}
+                />
+              </div>
+            </>
+          )
+        }
+      </div>
     </div>
   )
 }
